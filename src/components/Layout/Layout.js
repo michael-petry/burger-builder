508dc50c18fe67663c4acff6965ab98ddb0b139e
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2021-01-23 21:38:36 -0500

    Add support for toggling/closing SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSidedrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSidedrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        console.log('test');
+        this.setState((prevState) => {
+            return { showSidedrawer: !prevState.showSidedrawer }
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar
+                    drawerToggleClicked={this.sideDrawerToggleHandler}
+                />
+                <SideDrawer 
+                    open={this.state.showSidedrawer} 
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
